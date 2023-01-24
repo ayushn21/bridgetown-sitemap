523854cacad390d8af56dd143ca21572537b00c0
Ayush Newatia <ayush@hey.com>
2023-01-24 19:41:40 +0000

    Remove resource engine check

--- lib/bridgetown-sitemap/builder.rb
+++ lib/bridgetown-sitemap/builder.rb
@@ -3,17 +3,9 @@
 require "fileutils"
 
 module BridgetownSitemap
-  class UnsupportedContentEngine < StandardError; end
-
   class Builder < Bridgetown::Builder
     def build
       hook :site, :pre_render, priority: :low do |site|
-        unless site.uses_resource?
-          Bridgetown.logger.error "\n\nbridgetown-sitemap only supports the resource content engine"
-          Bridgetown.logger.info "Add `content_engine: 'resource'` to your bridgetown.config.yml\n\n"
-          raise UnsupportedContentEngine
-        end
-
         @site = site
 
         @site.generated_pages << sitemap unless file_exists?("sitemap.xml")
