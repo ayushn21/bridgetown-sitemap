7fae1d146b38d1ec7693074c1d26f346f38b8a87
seroperson <seroperson@gmail.com>
2025-02-22 14:17:09 +0300

    i18n support

--- test/fixtures/plugins/builders/generated_pages.rb
+++ test/fixtures/plugins/builders/generated_pages.rb
@@ -5,7 +5,13 @@ class Builders::GeneratedPages < SiteBuilder
       generated_page.content = "<%= 'created from a plugin'.capitalize %>"
       generated_page.data.layout = "default"
 
+      generated_page_ru = Bridgetown::GeneratedPage.new(site, site.source, "/ru", "generated_page.erb")
+      generated_page_ru.content = "<%= '(RU) created from a plugin'.capitalize %>"
+      generated_page_ru.data.layout = "default"
+      generated_page_ru.data.locale = :ru
+
       site.generated_pages << generated_page
+      site.generated_pages << generated_page_ru
     end
   end
 end
